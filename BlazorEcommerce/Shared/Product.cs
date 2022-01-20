c66155be2f193cee0ceab797de60cc6984e04f41
razvanr91 <razvanroman1991@gmail.com>
2022-01-20 01:52:35 +0000

    Create first migration

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace BlazorEcommerce.Shared
 
         public string ImageUrl { get; set; } = string.Empty;
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
