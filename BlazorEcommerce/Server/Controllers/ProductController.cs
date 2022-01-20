6d456def329235402bf99877683baa5211cb27a9
razvanr91 <razvanroman1991@gmail.com>
2022-01-20 23:44:26 +0000

    Create ServiceResponse

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -15,10 +15,14 @@ namespace BlazorEcommerce.Server.Controllers
         }
 
 		[HttpGet]
-		public async Task<ActionResult<List<Product>>> GetProducts()
+		public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
             var products = await _context.Products.ToListAsync();
-			return Ok(products);
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = products
+            };
+			return Ok(response);
         }
 	}
 }
