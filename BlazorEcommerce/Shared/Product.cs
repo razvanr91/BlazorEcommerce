3f9ed708b94c42d95e92f130298c5f1b355f3d78
razvanr91 <razvanroman1991@gmail.com>
2022-01-21 23:22:12 +0000

    Create Category model

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -19,5 +19,9 @@ namespace BlazorEcommerce.Shared
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        public Category? Category { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
